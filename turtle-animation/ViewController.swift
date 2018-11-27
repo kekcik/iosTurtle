bf6e8489851d10127ae7d59a8b0485bd7f387e9e
Таисия Пенская <tayaaa@icloud.com>
2018-11-27 15:13:48 +0300

    Initial Commit

---
+++ turtle-animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  turtle-animation
+//
+//  Created by Taya Penskaya on 27/11/2018.
+//  Copyright © 2018 Taya Penskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
